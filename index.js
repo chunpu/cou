1c47a2f1eaf0ac6c1bb7c783dc0b87d9a0415e39
chunpu <527653908@qq.com>
2015-06-09 18:06:24 +0800

    can work!

--- index.js
+++ index.js
@@ -4,73 +4,9 @@ var _ = exports
 
 _.is = is
 
-/*
-var stopKey = 'stopOnFalse'
-
-function each(arr, fn, custom) {
-	if (!is.fn(fn) || !is.arraylike(arr)) return
-
-	var len = arr.length
-	var opt = extend({}, custom)
-
-	if (custom) {
-		var ints = ['from', 'end', 'step']
-		for (var i = 0; i < ints.length; i++) {
-			var val = +opt[ints[i]]
-			if (!is.int(val)) val = undefined
-			opt[ints[i]] = val
-		}
-	}
-
-	var from = opt.from || 0
-	var end = opt.end || len
-	var step = opt.step || 1
-
-	if (custom) {
-		if (from < 0) from = 0
-		if (end > len) end = len
-		if (from + step * Infinity <= end) return arr // cannot finish
-	}
-
-	for (var i = from; i < end; i += step) {
-		var ret
-		if (opt.context) {
-			ret = fn.call(opt.context, arr[i], i, arr)
-		} else {
-			ret = fn(arr[i], i, arr)
-		}
-		// default is stop on false
-		if (false !== opt[stopKey] && false === ret) break
-	}
-
-	return arr
-}
-*/
-function each(arr, fn, start, end) {
-	var len = arr ? arr.length : 0
-	if (len && fn && 'call' in fn) {
-		start = start || 0
-		end = end || len
-		for (var i = start; i < end; i++) {
-			if (false === fn())
-		}
-	}
-}
+_.extend = extend
 
 _.each = each
-
-_.findIndex = findIndex
-
-function findIndex(arr, fn) {
-	var ret = -1
-	each(arr, function(item, i, arr) {
-		if (fn(item, i, arr)) {
-			ret = i
-			return false
-		}
-	})
-	return ret
-}
 
 _.map = function(arr, fn) {
 	var ret = []
@@ -93,15 +29,13 @@ _.some = function(arr, fn) {
 	return -1 != findIndex(arr, fn)
 }
 
-function negate(fn) {
-	return function() {
-		return !fn.apply(this, arguments)
-	}
-}
-
 _.every = function(arr, fn) {
 	return -1 == findIndex(arr, negate(fn))
 }
+
+_.reduce = reduce
+
+_.findIndex = findIndex
 
 _.find = function(arr, fn) {
 	var index = _.findIndex(arr, fn)
@@ -110,83 +44,17 @@ _.find = function(arr, fn) {
 	}
 }
 
-_.without = function(arr) {
-	var other = _.slice(arguments, 1)
-	return _.difference(arr, other)
-}
+_.indexOf = indexOf
 
-_.difference = function(arr, other) {
-	return _.filter(arr, function(item) {
-		return !_.has(other, item)
-	})
-}
-
-function slice(arr, from, end) {
-	var ret = []
-	each(arr, function(item) {
-		ret.push(item)
-	}, {
-		  from: from
-		, end: end
-	})
-	return ret
-}
-
-_.slice = slice
-
-function indexOf(val, sub) {
-	if (is.str(val)) return val.indexOf(sub)
-
-	return findIndex(val, function(item) {
-		return sub == item
-	})
-}
-
-function has(val, sub) {
+_.has = function(val, sub) {
 	return -1 != indexOf(val, sub)
 }
 
-_.has = has
+_.toArray = toArray
 
-function reduce(arr, fn, prev) {
-	each(arr, function(item, i) {
-		prev = fn(prev, item, i, arr)
-	})
-	return prev
-}
+_.slice = slice
 
-_.reduce = reduce
-
-_.only = function(obj, keys) {
-	obj = obj || {}
-	if (is.str(keys)) keys = keys.split(/ +/)
-	return reduce(keys, function(ret, key) {
-		if (null != obj[key]) ret[key] = obj[key]
-		return ret
-	}, {})
-}
-
-
-// Object
-
-function extend(dst) {
-	var len = arguments.length
-	if (dst && len > 1) {
-		for (var i = 1; i < len; i++) {
-			var hash = arguments[i]
-			if (hash) {
-				for (var key in hash) {
-					if (is.owns(hash, key)) {
-						var val = hash[key]
-						if (is.undef(val) || val === dst[key] || val === dst) continue
-						dst[key] = val
-					}
-				}
-			}
-		}
-	}
-	return dst
-}
+_.negate = negate
 
 _.keys = function(hash) {
 	var ret = []
@@ -200,8 +68,6 @@ _.keys = function(hash) {
 	return ret
 }
 
-// String
-
 var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g
 
 _.trim = function(str) {
@@ -210,3 +76,81 @@ _.trim = function(str) {
 }
 
 _.noop = function() {}
+
+function getLength(arr) {
+	if (null != arr) return arr.length
+}
+
+function each(arr, fn, from, end) {
+	var len = getLength(arr)
+	if (len && is.fn(fn)) {
+		from = from || 0
+		end = end || len
+		for (var i = from; i < end; i++) {
+			if (false === fn(arr[i], i, arr)) return
+		}
+	}
+}
+
+function findIndex(arr, fn) {
+	var ret = -1
+	each(arr, function(item, i, arr) {
+		if (fn(item, i, arr)) {
+			ret = i
+			return false
+		}
+	})
+	return ret
+}
+
+function toArray(arr) {
+	var ret = []
+	each(arr, function(item) {
+		ret.push(item)
+	})
+	return ret
+}
+
+function slice(arr, start, end) {
+	arr = toArray(arr)
+	start = start || 0
+	end = end || arr.length
+	return arr.slice(start, end)
+}
+
+function extend(target) {
+	var sources = slice(arguments, 1)
+	if (target) {
+		each(sources, function(src) {
+			each(_.keys(src), function(key) {
+				var val = src[key]
+				if (!is.undef(val)) {
+					target[key] = val
+				}
+			})
+		})
+	}
+	return target
+}
+
+function negate(fn) {
+	return function() {
+		return !fn.apply(this, arguments)
+	}
+}
+
+function indexOf(val, sub) {
+	if (is.str(val)) return val.indexOf(sub)
+
+	return findIndex(val, function(item) {
+		return sub == item
+	})
+}
+
+function reduce(arr, fn, prev) {
+	each(arr, function(item, i) {
+		prev = fn(prev, item, i, arr)
+	})
+	return prev
+}
+
--- test.js
+++ test.js
@@ -1,7 +1,33 @@
 var _ = require('./')
 var assert = require('assert')
 
-// Iteration
+describe('extend', function() {
+	it('should basic extend', function() {
+		var a = {a: 1, b: 2}
+		var ret = _.extend(a, {b: 3, c: undefined}, {d: 5})
+		assert(ret === a)
+		assert.deepEqual(ret, {a: 1, b: 3, d: 5})
+	})
+	it('always return first', function() {
+		var arr = [0, null, {}, false]
+		for (var i = 0; i < arr.length; i++) {
+			assert.deepEqual(_.extend(arr[i]), arr[i])
+		}
+	})
+	it('not crash when get shit', function() {
+		var ret = _.extend(null, {foo: true})
+		assert.deepEqual(ret, null)
+	})
+	it('should work on this', function() {
+		var obj = {
+			add: function(val) {
+				_.extend(this, val)
+			}
+		}
+		obj.add({foo: true})
+		assert(true == obj.foo)
+	})
+})
 
 describe('each', function() {
 	it('never run empty', function() {
@@ -25,16 +51,6 @@ describe('each', function() {
 	it('cannot crash when fn is shit', function() {
 		_.each([1, 2, 3])
 		assert(true)
-	})
-	it('run belong 0 ~ arr.length', function() {
-		var arr = []
-		_.each([1, 2, 3], function(val) {
-			arr.push(val)
-		}, {
-			start: -100,
-			end: 100
-		})
-		assert.deepEqual([1, 2, 3], arr)
 	})
 })
 
@@ -68,6 +84,30 @@ describe('some', function() {
 	})
 })
 
+describe('every', function() {
+	it('should return true', function() {
+		assert(true === _.every([1, 2, 3, 4, 5], function(x) {
+			return x > 0
+		}))
+	})
+	it('should return false', function() {
+		assert(false === _.every([1, 2, 3, 4, 5], function(x) {
+			return x > 3
+		}))
+	})
+})
+
+describe('reduce', function() {
+	it('can realize sum', function() {
+		function sum(arr) {
+			return _.reduce(arr, function(ret, val) {
+				return ret + val
+			}, 0)
+		}
+		assert(10 == sum([1, 2, 3, 4]))
+	})
+})
+
 describe('findIndex', function() {
 	it('should find first matched item', function() {
 		assert.equal(2, _.findIndex([1, 2, 3, 4, 5], function(x) {
@@ -94,30 +134,102 @@ describe('find', function() {
 	})
 })
 
-describe('every', function() {
+describe('indexOf', function() {
+	it('should return index', function() {
+		assert(-1 == _.indexOf([1, 2, 3], 4))
+		assert(1 == _.indexOf([1, 2, 3], 2))
+	})
+})
+
+describe('toArray', function() {
+	it('should turn array like to array', function() {
+		var is = require('min-is')
+		function test() {
+			var arr = _.toArray(arguments)
+			assert(is.array(arr))
+			assert(arguments.length == arr.length)
+		}
+
+		test()
+		test(1, 2, 3)
+	})
+	it('should always return a new array', function() {
+		var arr = [1, 2, 3]
+		var arr2 = _.toArray(arr)
+		assert(arr2 != arr)
+		assert.deepEqual(arr2, arr)
+	})
+})
+
+describe('has', function() {
 	it('should return true', function() {
-		assert(true === _.every([1, 2, 3, 4, 5], function(x) {
-			return x > 0
-		}))
+		assert(true === _.has('qwer', 'we'))
+		assert(true === _.has([1, 2, 3], 2))
 	})
 	it('should return false', function() {
-		assert(false === _.every([1, 2, 3, 4, 5], function(x) {
-			return x > 3
-		}))
+		assert(false === _.has('qwer', 'ew'))
+		assert(false === _.has([1, 2, 3], 4))
 	})
 })
 
-describe('difference', function() {
-	it('should return new array without other', function() {
-		var arr = _.difference([1, 2, 3, 1, 2, 3], [1, 2])
-		assert.deepEqual([3, 3], arr)
+describe('slice', function() {
+	it('should create a new array', function() {
+		var arr = [1, 2, 3, 4]
+		var slice = _.slice(arr)
+		assert(arr !== slice)
+		assert.deepEqual(arr, slice)
+	})
+	it('should return rest when only giving begin', function() {
+		var arr = [1, 2, 3, 4]
+		var slice = _.slice(arr, 1)
+		assert.deepEqual(slice, [2, 3, 4])
+	})
+	it('should return empty array when param is shit', function() {
+		assert.deepEqual(_.slice(), [])
+		assert.deepEqual(_.slice([1, 2, 3], 3, 2), [])
+	})
+	it('should auto adjust when out of range', function() {
+		assert.deepEqual(_.slice([1, 2, 3], -1, 100), [3])
 	})
 })
 
-describe('without', function() {
-	it('should return new array without args', function() {
-		var arr = _.without([1, 2, 3, 1, 2], 1)
-		assert.deepEqual([2, 3, 2], arr)
+describe('negate', function() {
+	it('always return negate', function() {
+		function isMatch3(val) {
+			return 3 == val
+		}
+		var isNotMatch3 = _.negate(isMatch3)
+		assert(true == isNotMatch3(2))
+		assert(false == isNotMatch3(3))
 	})
 })
 
+describe('keys', function() {
+	it('should return key in hash, and hash owns key', function() {
+		function Ctor() {}
+		Ctor.prototype.foo = 'bar'
+		var hash = new Ctor
+		hash.key = 'val'
+		assert.deepEqual(_.keys(hash), ['key'])
+	})
+})
+
+describe('trim', function() {
+	it('should ok', function() {
+		assert(_.trim('  qq  ') == 'qq')
+		assert(_.trim('   ') === '')
+		assert(_.trim('') === '')
+		assert(_.trim(true) === 'true')
+		assert.equal(_.trim(NaN), 'NaN')
+	})
+	it('should return empty when meet null', function() {
+		assert(_.trim() === '')
+		assert(_.trim(null) === '')
+	})
+})
+
+describe('noop', function() {
+	it('always do nothing', function() {
+		assert(undefined === _.noop())
+	})
+})
